10960ecdbcd13552e1a162af13dafaea51e79dc4
Mac <macelux@hotmail.com>
2021-04-20 22:30:31 +0100

    Add entity framework

--- sampleApu/EmployeeData/SqlEmployeeData.cs
+++ sampleApu/EmployeeData/SqlEmployeeData.cs
@@ -1,10 +1,55 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using sampleApu.Models;
+
 namespace sampleApu.EmployeeData
 {
-    public class SqlEmployeeData
+    public class SqlEmployeeData : IEmployeeInterface
     {
-        public SqlEmployeeData()
+
+        private EmployeeContext _employeeContext;
+
+        public SqlEmployeeData(EmployeeContext employeeContext)
         {
+            _employeeContext = employeeContext;
         }
+
+        public Employee AddEmployee(Employee employee)
+        {  
+            _employeeContext.Add(employee);
+            _employeeContext.SaveChanges();
+            return employee;
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            var employee = _employeeContext.Employees.Find(id);
+            return employee;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return _employeeContext.Employees.ToList();
+        }
+
+        public string UpdateEmployee(Guid id , Employee employee)
+        { 
+            var result = _employeeContext.Employees.Find(id);
+            if (result != null)
+            {
+                result.Name = employee.Name;
+                _employeeContext.SaveChanges();
+                return "true";
+            }
+
+            return "false";
+        }
+
     }
 }
